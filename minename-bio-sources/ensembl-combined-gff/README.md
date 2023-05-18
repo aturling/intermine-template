58cbc39293423720055b82368ed0c3c2d4015397
Amy Walsh <aturling@users.noreply.github.com>
2023-05-18 14:58:32 -0500

    Adding RefSeq/Ensembl gene+pseudogene combined gff sources

---
+++ minename-bio-sources/ensembl-combined-gff/README.md
@@ -0,0 +1,4 @@
+This loader can be used for Ensembl gff files with both genes and pseudogenes combined.
+
+This source was developed but never actually used; archiving for future reference.
+
---
+++ minename-bio-sources/refseq-combined-gff/src/main/java/org/intermine/bio/dataconversion/RefSeqGFF3RecordHandler.java
@@ -0,0 +1,137 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2022 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.intermine.bio.io.gff3.GFF3Record;
+import org.intermine.metadata.Model;
+import org.intermine.metadata.StringUtil;
+import org.intermine.xml.full.Item;
+
+/**
+ * A converter/retriever for RefSeq GFF files.
+ */
+
+public class RefSeqGFF3RecordHandler extends BaseGFF3RecordHandler
+{
+    /**
+     * Create a new RefSeqGFF3RecordHandler for the given data model.
+     * @param model the model for which items will be created
+     */
+    public RefSeqGFF3RecordHandler (Model model) {
+        super(model);
+
+        // relationship: exon <-> transcript <-> gene
+        refsAndCollections.put("Transcript", "gene");
+        refsAndCollections.put("CDS", "transcript");
+        refsAndCollections.put("Exon", "transcripts");
+
+        // relationship: transcribed_pseudogenic_exon <-> pseudogenic_transcript <-> transcribed_pseudogene
+        refsAndCollections.put("PseudogenicTranscript", "gene");
+        refsAndCollections.put("TranscribedPseudogenicExon", "transcripts");
+
+        // relationship: nontranscribed_pseudogenic_exon <-> nontranscribed_pseudogene
+        refsAndCollections.put("NontranscribedPseudogenicExon", "gene");        
+
+        // Note: these may change with each release depending on the feature classes in the GFF files.
+        // Comment out lines that don't apply to this mine release.
+        refsAndCollections.put("AntisenseRNA", "gene");
+        refsAndCollections.put("CGeneSegment", "gene");
+        refsAndCollections.put("DGeneSegment", "gene");
+        // DLoop has no parent
+        refsAndCollections.put("GuideRNA", "gene");
+        // Insulator has no parent
+        refsAndCollections.put("JGeneSegment", "gene");
+        refsAndCollections.put("LncRNA", "gene");
+        refsAndCollections.put("MiRNA", "transcript");
+        refsAndCollections.put("MRNA", "gene");
+        refsAndCollections.put("NcRNA", "gene");
+        // OriginOfReplication has no parent
+        refsAndCollections.put("PrimaryTranscript", "gene");
+        refsAndCollections.put("PseudogenicRRNA", "gene");
+        refsAndCollections.put("PseudogenicTRNA", "gene");
+        refsAndCollections.put("RNaseMRPRNA", "gene");
+        refsAndCollections.put("RNasePRNA", "gene");
+        refsAndCollections.put("RRNA", "gene");
+        refsAndCollections.put("ScRNA", "gene");
+        refsAndCollections.put("SnoRNA", "gene");
+        refsAndCollections.put("SnRNA", "gene");
+        refsAndCollections.put("SRPRNA", "gene");
+        // TandemRepeat has no parent
+        refsAndCollections.put("TelomeraseRNA", "gene");
+        refsAndCollections.put("TRNA", "gene");
+        refsAndCollections.put("VaultRNA", "gene");
+        refsAndCollections.put("VGeneSegment", "gene");
+        refsAndCollections.put("YRNA", "gene");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void process(GFF3Record record) {
+        super.process(record);
+
+        Item feature = getFeature();
+        String clsName = feature.getClassName();
+
+        // Set symbol, description, transcript biotype for all features (if present)
+        // (CDS, Exon won't have symbol or description, Gene won't have transcript biotype)
+        setFeatureSymbol(record, "symbol_ncbi");
+        setFeatureDescription(record);
+        setFeatureBiotype(record, "transcript_biotype");
+
+        // Set note, exception if present
+        setFeatureAttribute(record, "Note", "note");
+        setFeatureAttribute(record, "exception", "exceptionNote");
+  
+        // Set status (ambiguous/frameshift cases only)
+        // setFeatureStatus(record);
+ 
+        // Update primary identifier to "RefSeq_NA" key value, if present (applies to transcript, ncRNA, mRNA, rRNA, etc.)
+        handleDbxrefs(record, "primaryIdentifier", "RefSeq_NA");
+
+        // Extra processing according to class
+        // Not every mine will have all of these classes; class names not present in this mine will be ignored.
+        if (clsName.equals("Gene") || clsName.equals("TranscribedPseudogene") || clsName.equals("NontranscribedPseudogene"))  {
+            // Update primary identifier according to "NCBI_Gene" key value
+            handleDbxrefs(record, "primaryIdentifier", "NCBI_Gene");
+
+            // Set gene biotype
+            setFeatureBiotype(record, "gene_biotype");
+
+            // Handle duplicate entities, if applicable
+            if (converter.getLoadDuplicateEntities()) {
+                createDuplicateEntities(record);
+            }
+
+            // Set SO terms
+            if (clsName.equals("TranscribedPseudogene") || clsName.equals("NontranscribedPseudogene")) {
+                setFeatureSOTerm("pseudogene");
+            }
+
+        } else if (clsName.equals("Transcript") || clsName.equals("MRNA") || clsName.equals("PseudogenicTranscript")) {
+            // Set protein identifier
+            handleDbxrefs(record, "proteinIdentifier", "RefSeq_Prot");
+
+        } else if (clsName.equals("PrimaryTranscript") || clsName.equals("MiRNA")) {
+            // Set miRBase identifier
+            handleDbxrefs(record, "mirbaseIdentifier", "miRBase");
+
+        } else if (clsName.equals("TranscribedPseudogenicExon") || clsName.equals("NontranscribedPseudogenicExon")) {
+            // Set SO terms
+            setFeatureSOTerm("pseudogenic_exon");
+        }
+    }
+}
